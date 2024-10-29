9fde5054b68bb3ae8f71dd3aec273fe0b5c316e5
yqx <1173200036@qq.com>
2024-10-29 11:58:41 +0800

    apply symmetric rule to map from mis326 to mis316

--- BC-W4.m
+++ BC-W4.m
@@ -1,29 +1,26 @@
 (* Created with the Wolfram Language : www.wolfram.com *)
 {(37*Pi^4)/480, (37*Pi^4)/480, (37*Pi^4)/480, (-67*Pi^4)/720, (17*Pi^4)/96, 
- (17*Pi^4)/96, 0, (-167*Pi^4)/960, (17*Pi^4)/96, (17*Pi^4)/96, 0, 
- (-167*Pi^4)/960, 0, 0, 0, (151*Pi^4)/360 + (4*Pi^2*Log[2]^2)/3 - 
-  (4*Log[2]^4)/3 - 32*PolyLog[4, 1/2], (-47*Pi^4)/160, 0, 0, 0, (17*Pi^4)/96, 
- (17*Pi^4)/96, (17*Pi^4)/96, (-211*Pi^4)/720, (17*Pi^4)/96, (17*Pi^4)/96, 
- (17*Pi^4)/96, (-211*Pi^4)/720, (-77*Pi^4)/1920, (-77*Pi^4)/1920, 
- (-77*Pi^4)/1920, (89*Pi^4)/960, (71*Pi^4)/160, (71*Pi^4)/160, 0, 
- (-67*Pi^4)/120, (11*Pi^4)/96, (11*Pi^4)/24, 0, 0, 0, 0, 
+ (17*Pi^4)/96, 0, (-167*Pi^4)/960, (17*Pi^4)/96, 0, (-167*Pi^4)/960, 0, 0, 0, 
+ (151*Pi^4)/360 + (4*Pi^2*Log[2]^2)/3 - (4*Log[2]^4)/3 - 32*PolyLog[4, 1/2], 
+ (-47*Pi^4)/160, 0, 0, 0, (17*Pi^4)/96, (17*Pi^4)/96, (-211*Pi^4)/720, 
+ (-77*Pi^4)/1920, (-77*Pi^4)/1920, (-77*Pi^4)/1920, (89*Pi^4)/960, 
+ (71*Pi^4)/160, (71*Pi^4)/160, 0, (-67*Pi^4)/120, (11*Pi^4)/24, 0, 0, 0, 0, 
  (71*Pi^4)/144 + (5*Pi^2*Log[2]^2)/3 - (5*Log[2]^4)/3 - 40*PolyLog[4, 1/2], 
  (-1889*Pi^4)/2880, 0, (61*Pi^4)/360 + (2*Pi^2*Log[2]^2)/3 - (2*Log[2]^4)/3 - 
-  16*PolyLog[4, 1/2], 0, 0, (71*Pi^4)/160, (71*Pi^4)/160, 0, (-67*Pi^4)/120, 
- 0, (11*Pi^4)/24, 0, 0, (71*Pi^4)/144 + (5*Pi^2*Log[2]^2)/3 - 
-  (5*Log[2]^4)/3 - 40*PolyLog[4, 1/2], (-1889*Pi^4)/2880, 0, (11*Pi^4)/96, 0, 
- 0, 0, (61*Pi^4)/360 + (2*Pi^2*Log[2]^2)/3 - (2*Log[2]^4)/3 - 
-  16*PolyLog[4, 1/2], 0, 0, 0, (-73*Pi^4)/480, 
- (17*Pi^4)/30 + 2*Pi^2*Log[2]^2 - 2*Log[2]^4 - 48*PolyLog[4, 1/2], 
- (-263*Pi^4)/240, 0, -1/10*Pi^4, 0, (-73*Pi^4)/480, 0, (-7*Pi^4)/180, 0, 0, 
- (4939*Pi^4)/2880 + 6*Pi^2*Log[2]^2 - 6*Log[2]^4 - 144*PolyLog[4, 1/2], 0, 0, 
- 0, 0, (-43*Pi^4)/720, 0, 0, 0, 0, 0, (4939*Pi^4)/2880 + 6*Pi^2*Log[2]^2 - 
-  6*Log[2]^4 - 144*PolyLog[4, 1/2], 0, (-43*Pi^4)/720, (71*Pi^4)/160, 
- (71*Pi^4)/160, 0, (-67*Pi^4)/120, (7*Pi^4)/864, 0, (-337*Pi^4)/1080, 
- (-337*Pi^4)/1080, (-67*Pi^4)/120, (-19*Pi^4)/10 + 90*Log[GoldenRatio]^4 + 
-  90*Log[GoldenRatio]^2*PolyLog[2, -GoldenRatio] + 
-  90*PolyLog[4, GoldenRatio^(-1)] - 90*PolyLog[4, -GoldenRatio], 
- (-551*Pi^4)/720 + (172*Log[GoldenRatio]^4)/3 + 
+  16*PolyLog[4, 1/2], 0, 0, (71*Pi^4)/160, 0, (-67*Pi^4)/120, 0, 
+ (11*Pi^4)/24, 0, 0, (71*Pi^4)/144 + (5*Pi^2*Log[2]^2)/3 - (5*Log[2]^4)/3 - 
+  40*PolyLog[4, 1/2], (-1889*Pi^4)/2880, 0, (11*Pi^4)/96, 0, 0, 0, 
+ (61*Pi^4)/360 + (2*Pi^2*Log[2]^2)/3 - (2*Log[2]^4)/3 - 16*PolyLog[4, 1/2], 
+ 0, 0, 0, (-73*Pi^4)/480, (17*Pi^4)/30 + 2*Pi^2*Log[2]^2 - 2*Log[2]^4 - 
+  48*PolyLog[4, 1/2], (-263*Pi^4)/240, 0, -1/10*Pi^4, 0, (-73*Pi^4)/480, 0, 
+ (-7*Pi^4)/180, 0, 0, (4939*Pi^4)/2880 + 6*Pi^2*Log[2]^2 - 6*Log[2]^4 - 
+  144*PolyLog[4, 1/2], 0, 0, 0, 0, (-43*Pi^4)/720, 0, 0, 0, 0, 0, 
+ (4939*Pi^4)/2880 + 6*Pi^2*Log[2]^2 - 6*Log[2]^4 - 144*PolyLog[4, 1/2], 0, 
+ (-43*Pi^4)/720, (71*Pi^4)/160, (71*Pi^4)/160, 0, (-67*Pi^4)/120, 
+ (7*Pi^4)/864, 0, (-337*Pi^4)/1080, (-337*Pi^4)/1080, (-67*Pi^4)/120, 
+ (-19*Pi^4)/10 + 90*Log[GoldenRatio]^4 + 90*Log[GoldenRatio]^2*
+   PolyLog[2, -GoldenRatio] + 90*PolyLog[4, GoldenRatio^(-1)] - 
+  90*PolyLog[4, -GoldenRatio], (-551*Pi^4)/720 + (172*Log[GoldenRatio]^4)/3 + 
   (200*Log[GoldenRatio]^2*PolyLog[2, -GoldenRatio])/3 - 
   56*PolyLog[4, GoldenRatio^(-1)] - 56*PolyLog[4, -GoldenRatio], 0, 
  -1/240*Pi^4, (-25*Pi^4)/576, (151*Pi^4)/720 + (2*Pi^2*Log[2]^2)/3 - 
@@ -33,20 +30,16 @@
   (43*Log[GoldenRatio]^4)/12 - 
   (25*Log[GoldenRatio]^2*PolyLog[2, -GoldenRatio])/6 + 
   (7*PolyLog[4, GoldenRatio^(-1)])/2 + (7*PolyLog[4, -GoldenRatio])/2, 0, 0, 
- (-25*Pi^4)/576, 0, (11*Pi^4)/360, (151*Pi^4)/720 + (2*Pi^2*Log[2]^2)/3 - 
-  (2*Log[2]^4)/3 - 16*PolyLog[4, 1/2], (-2401*Pi^4)/5760, 
- (1171*Pi^4)/2880 + (4*Pi^2*Log[2]^2)/3 - (4*Log[2]^4)/3 - 
-  32*PolyLog[4, 1/2], -1/240*Pi^4, (-17*Pi^4)/720 - 
-  (43*Log[GoldenRatio]^4)/12 - 
+ 0, (11*Pi^4)/360, (151*Pi^4)/720 + (2*Pi^2*Log[2]^2)/3 - (2*Log[2]^4)/3 - 
+  16*PolyLog[4, 1/2], (-2401*Pi^4)/5760, (1171*Pi^4)/2880 + 
+  (4*Pi^2*Log[2]^2)/3 - (4*Log[2]^4)/3 - 32*PolyLog[4, 1/2], -1/240*Pi^4, 
+ (-17*Pi^4)/720 - (43*Log[GoldenRatio]^4)/12 - 
   (25*Log[GoldenRatio]^2*PolyLog[2, -GoldenRatio])/6 + 
   (7*PolyLog[4, GoldenRatio^(-1)])/2 + (7*PolyLog[4, -GoldenRatio])/2, 0, 0, 
  (-229*Pi^4)/960, (71*Pi^4)/640, (-229*Pi^4)/960, (71*Pi^4)/640, 0, 0, 0, 
  (59*Pi^4)/90 + 2*Pi^2*Log[2]^2 - 2*Log[2]^4 - (43*Log[GoldenRatio]^4)/2 - 
   25*Log[GoldenRatio]^2*PolyLog[2, -GoldenRatio] - 48*PolyLog[4, 1/2] + 
-  21*PolyLog[4, GoldenRatio^(-1)] + 21*PolyLog[4, -GoldenRatio], 
- (19*Pi^4)/10 - 90*Log[GoldenRatio]^4 - 90*Log[GoldenRatio]^2*
-   PolyLog[2, -GoldenRatio] - 90*PolyLog[4, GoldenRatio^(-1)] + 
-  90*PolyLog[4, -GoldenRatio] + 2*trans[15], 
+  21*PolyLog[4, GoldenRatio^(-1)] + 21*PolyLog[4, -GoldenRatio], BC132W4, 
  (1627*Pi^4)/1152 + 4*Pi^2*Log[2]^2 - 4*Log[2]^4 - 
   (172*Log[GoldenRatio]^4)/3 - 
   (200*Log[GoldenRatio]^2*PolyLog[2, -GoldenRatio])/3 - 96*PolyLog[4, 1/2] + 
@@ -151,7 +144,7 @@
   7*PolyLog[4, GoldenRatio^(-1)] + 7*PolyLog[4, -GoldenRatio], 
  (7*Pi^4)/72 + (Pi^2*Log[2]^2)/3 - Log[2]^4/3 - (43*Log[GoldenRatio]^4)/6 - 
   (25*Log[GoldenRatio]^2*PolyLog[2, -GoldenRatio])/3 - 8*PolyLog[4, 1/2] + 
-  7*PolyLog[4, GoldenRatio^(-1)] + 7*PolyLog[4, -GoldenRatio], 0, 0, 0, 
+  7*PolyLog[4, GoldenRatio^(-1)] + 7*PolyLog[4, -GoldenRatio], 0, 0, 
  (-19*Pi^4)/1080, 0, 0, (29*Pi^4)/432, Pi^4/30, (-103*Pi^4)/320, 
  (61*Pi^4)/720 + (Pi^2*Log[2]^2)/3 - Log[2]^4/3 - 8*PolyLog[4, 1/2], 0, 
  (-61*Pi^4)/720 - (Pi^2*Log[2]^2)/3 + Log[2]^4/3 + 8*PolyLog[4, 1/2], 
